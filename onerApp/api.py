3bad773892ef156a7303156342629180cba2ce61
Murad-Awad <murad.bummos@gmail.com>
2020-04-18 22:46:11 -0700

    added webscraping to the backend

--- onerApp/api.py
+++ onerApp/api.py
@@ -1,6 +1,13 @@
 from .models import Clothing
 from rest_framework import viewsets, permissions
 from .serializers import ClothingSerializer
+from bs4 import BeautifulSoup, Comment, NavigableString
+import requests
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.exceptions import ValidationError, ParseError
+import logging
+import json
 
 #Clothing Viewset
 class ClothingViewSet(viewsets.ModelViewSet):
@@ -9,3 +16,74 @@ class ClothingViewSet(viewsets.ModelViewSet):
 		permissions.AllowAny
 	]
 	serializer_class = ClothingSerializer
+
+
+#web scraping get request
+@api_view(['GET'])
+def searchForClothesPoshmark(request):
+	logger = logging.getLogger('mylogger')
+	try:
+		body = json.loads(request.body)
+		query = body["query"]
+		searchResultsObject = {"data": []}
+		poshmarkResults = requests.get("https://poshmark.com/search?query=" + query).text
+		soup = BeautifulSoup(poshmarkResults)
+		soup = soup.find("div", {"data-test": "tiles_container"})
+		soup = soup.contents
+		for listing in soup:
+			listingToAdd = extractInfo(listing)
+			if listingToAdd:
+				searchResultsObject["data"].append(listingToAdd)
+		return JsonResponse(searchResultsObject)
+	except ValueError:
+		raise ParseError
+
+
+def extractInfo(listing):
+	listingToAdd = None
+	if type(listing) is not Comment and listing.get("class")[0] == 'tile':
+		listingToAdd = {}
+		listingContents = {}
+		listingDetails = listing.find("div", class_="item__details")
+		listingContents["name"] = getNameFromPoshmark(listingDetails)
+		listingContents["img"] = getImageFromPoshmark(listing)
+		listingContents["price"] = getPriceFromPoshmark(listingDetails)
+		listingContents["originalPrice"] = getOriginalPriceFromPoshmark(listingDetails)
+		image_tree_link = listing.find("div", class_="img__container img__container--square")
+		image = image_tree_link.find("img")['src']
+		listingDetails = listing.find("div", class_="item__details")
+		name = listingDetails.find("a", {"data-et-name": "listing"})
+		currentPrice = listingDetails.find("span", {"data-test": "tile-price"})
+		originalPrice = listingDetails.find("span", {"data-test": "tile-original-price"})
+		listingToAdd["listing"] = listingContents
+	return listingToAdd
+
+def getImageFromPoshmark(listing):
+	image_tree_link = listing.find("div", class_="img__container img__container--square")
+	image = image_tree_link.find("img") if image_tree_link else None
+	imageToReturn = image["src"] if image else None
+	return image["data-src"] if image and not imageToReturn else imageToReturn
+
+def getNameFromPoshmark(listingDetails):
+	name = None
+	if listingDetails:
+		name = listingDetails.find("a", {"data-et-name": "listing"})
+	return stringPrettify(name.text) if name else None
+
+def getPriceFromPoshmark(listingDetails):
+	price = None
+	if listingDetails:
+		price = listingDetails.find("span", {"data-test": "tile-price"})
+	return stringPrettify(price.text) if price else None
+
+def getOriginalPriceFromPoshmark(listingDetails):
+	price = None
+	if listingDetails:
+		price = listingDetails.find("span", {"data-test": "tile-original-price"})
+	return stringPrettify(price.text) if price else None
+
+def stringPrettify(text):
+	return text.lstrip().rstrip()
+
+
+
--- onerApp/urls.py
+++ onerApp/urls.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
 from .api import ClothingViewSet
+from .api import searchForClothesPoshmark
+from django.urls import path
 
 router = routers.DefaultRouter()
 router.register('api/clothes', ClothingViewSet, 'clothes')
-
-urlpatterns = router.urls
+urlpatterns = router.urls + [path('api/query', searchForClothesPoshmark)]
